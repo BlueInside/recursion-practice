9b9071f26ddb2b964236f71af433a77e8fe66e87
Karol <kpulawski13@gmail.com>
2023-08-07 09:55:02 +0100

    Array that calls cb on every value

--- practice.js
+++ practice.js
@@ -1,8 +1,7 @@
-function all(arr, callback) {
-  let arrCopy = [...arr];
+function all(arr, callback, arrCopy = [...arr]) {
   if (!arrCopy[0]) return true;
   if (callback(arrCopy[0])) {
-    arrCopy.shift();
-    return all(arrCopy[0], callback);
-  } else return false;
+    return all(arr, callback, arrCopy.slice(1));
+  }
+  return false;
 }
