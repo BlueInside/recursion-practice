44ea8de416b5acba61b3c491ed219a6891fa54d1
Karol <kpulawski13@gmail.com>
2023-08-07 13:03:36 +0100

    square of numbers in list that might contain more lists

--- practice.js
+++ practice.js
@@ -1,25 +1,9 @@
-const nestedObject = {
-  data: {
-    info: {
-      stuff: {
-        thing: {
-          moreStuff: {
-            magicNumber: 44,
-            something: 'foo2222',
-          },
-        },
-      },
-    },
-  },
-};
-
-function contains(objectToSearch, value) {
-  const keys = Object.keys(objectToSearch);
-  for (let key of keys) {
-    if (objectToSearch[key] === value) return true;
-    else if (typeof objectToSearch[key] === 'object') {
-      return contains(objectToSearch[key], value);
-    }
+function sumSquare(list, result = 0) {
+  if (Number.isInteger(list)) return (result += list ** 2);
+  if (Array.isArray(list)) {
+    list.forEach((element) => {
+      result = sumSquare(element, result);
+    });
   }
-  return false;
+  return result;
 }
